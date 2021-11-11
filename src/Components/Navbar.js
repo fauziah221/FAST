abfb3ad1d1cfa1eadcb723302f8898b047fdaeff
Emilysav <70286291+Emilysav@users.noreply.github.com>
2021-11-11 12:55:58 -0500

    navbar component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,31 +1,48 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css'
-import { Navbar, NavDropdown, Container, Nav } from 'react-bootstrap'
+import { Navbar, Container, Nav, NavItem } from 'react-bootstrap'
+import Home from '../Pages/Home';
+import Connect from '../Pages/Connect';
+import Challanges from '../Pages/Challanges';
+import About from '../Pages/About'
+import {BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
 
 function  navbar() {
     return (
-        <div className="navbar">
-            <Navbar bg="dark" expand="sm">
-                <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
+        <Router>
+            <div className="navbar">
+                <Navbar bg="dark" variant="dark" expand="sm" fixed="top">
+                    <Container >
+                        <Navbar.Brand >FAST</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav fill className="me-auto" fill={"true"}>
+                                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                                <Nav.Link as={Link} to="/Connect">Connect</Nav.Link>
+                                <Nav.Link as={Link} to="/Challanges">Challanges</Nav.Link>
+                                <Nav.Link as={Link} to="/About">About</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+            <div>
+            <Switch>
+                <Route exact path="/"> 
+                <Home/>
+                </Route>
+                <Route exact path="/Connect"> 
+                <Connect/>
+                </Route>
+                <Route exact path="/Challanges"> 
+                <Challanges/>
+                </Route>
+                <Route exact path="/About"> 
+                <About/>
+                </Route>
+          </Switch>
+            </div>
+        </Router>
     )}
 
 export default navbar;
