f279246f0d8f10a5029ec3a003eeeb977ade6ae5
Emilysav <70286291+Emilysav@users.noreply.github.com>
2021-11-13 15:13:38 -0500

    fixed navbar
    NAvbar properly covers the top of the screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 function App() {
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,41 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
-import { Navbar, Container, Nav, NavItem } from 'react-bootstrap'
-import Home from '../Pages/Home';
-import Connect from '../Pages/Connect';
-import Challanges from '../Pages/Challanges';
-import About from '../Pages/About'
-import {BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
+import { Navbar, Nav } from 'react-bootstrap'
+import {BrowserRouter as Router, Link } from "react-router-dom"
 
+//<img className="photo" src={FASTLOGO}></img>
 function  navbar() {
     return (
         <Router>
+            <div className="Navbar">
+                <Navbar bg="dark" variant="dark" expand="lg">
+                    <Navbar.Brand>
+                        FAST
+                    </Navbar.Brand>
+                    
+                    <Nav>
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/Connect">Connect</Nav.Link>
+                        <Nav.Link as={Link} to="/Challanges">Challanges</Nav.Link>
+                        <Nav.Link as={Link} to="/About">About</Nav.Link>
+                    </Nav>
+
+                </Navbar>
+            </div>
+        </Router>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*<Router>
             <div className="navbar">
-                <Navbar bg="dark" variant="dark" expand="sm" fixed="top">
+                <Navbar bg="dark" variant="dark" expand="sm" >
                     <Container >
                         <Navbar.Brand >FAST</Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -42,7 +66,7 @@ function  navbar() {
                 </Route>
           </Switch>
             </div>
-        </Router>
+        </Router>*/
     )}
 
 export default navbar;
